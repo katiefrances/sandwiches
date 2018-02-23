abc405154b2c3bcba4f72b4310d9b953cf423b0e
Katie Frances <katiebeeisdead@gmail.com>
2018-02-23 12:25:12 +1100

    Create Initial commit

---
+++ peanut-butter-sandwich.rb
@@ -0,0 +1,4 @@
+
+def make_peanut_butter_sandwich(number_sandwiches)
+	make_sandwich(type: "peanut butter", number_of_sandwiches: number_sandwiches)
+end
---
+++ vegemite-sandwich.rb
@@ -0,0 +1,4 @@
+
+def make_vegemite_sandwich(num)
+	make_sandwich(type: "vegemite", number_of_sandwiches: num)
+end
